5a5b52e238098e70665733b2912ecc6984150818
devxsby <devxsby@gmail.com>
2023-05-21 23:47:46 +0900

    chore: tabbar controller selected index service vc로 지정

--- BankApp/Projects/Features/RootFeature/Sources/SplashViewController.swift
+++ BankApp/Projects/Features/RootFeature/Sources/SplashViewController.swift
@@ -54,6 +54,7 @@ public class SplashViewController: UIViewController {
         }, completion: { finished in
             print("애니메이션 종료")
             let tabBarController = TabBarViewController()
+            tabBarController.selectedIndex = 1
             self.navigationController?.pushViewController(tabBarController, animated: true)
             let navigation = UINavigationController(rootViewController: tabBarController)
             SplashViewController.setRootViewController(window: self.view.window!, viewController: navigation, withAnimation: true)
